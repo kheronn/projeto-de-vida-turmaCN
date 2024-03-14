fe65f7159ccd4abd5f75438f7f5a9db2fa11d898
unknown <kheronn@gmail.com>
2024-03-14 14:24:45 -0300

    Commit formação do dia 13/03

---
+++ main.js
@@ -0,0 +1,11 @@
+const botoes = document.querySelectorAll(".botao");
+
+// Laço de repetição
+for(let i = 0; i < botoes.length; i++){
+    botoes[i].onclick = function(){
+        for(let j = 0; j < botoes.length; j++){
+            botoes[j].classList.remove("ativo");
+        }
+        botoes[i].classList.add("ativo");
+    }
+}

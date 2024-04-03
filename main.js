e4530b0682f32a38deccefd3ba12bf4bb830de16
unknown <kheronn@gmail.com>
2024-04-03 19:47:06 -0300

    Reunião 05:  Convertendo data

--- main.js
+++ main.js
@@ -14,5 +14,20 @@ for(let i = 0; i < botoes.length; i++){
 // document.querySelector("#minhaDiv p").textContent = "vai embora calor";
 const contadores = document.querySelectorAll(".contador");
 let tempoAtual = new Date();
-const tempoObjetivo1 = new Date("2024-10-20T00:00:00");
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+const tempoObjetivo1 = new Date("2024-05-03T00:00:00");
+
+function calculaTempo(tempoObjetivo){
+let tempoFinal = tempoObjetivo - tempoAtual;
+
+let segundos = Math.floor(tempoFinal / 1000);
+let minutos = Math.floor(segundos / 60);
+let horas = Math.floor(minutos / 60);
+let dias = Math.floor(horas / 24);
+
+segundos %= 60;
+minutos %= 60;
+horas %= 24;
+return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+}
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
